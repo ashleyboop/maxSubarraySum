99bca70c0d575c39ffffc2534f575599ac58ec31
ashleyboop <acuadra1@fau.edu>
2016-10-26 10:27:40 -0400

    Applied changes to main.cpp
    changes made

--- Max_Subarray_Sum/Max_Subarray_Sum/main.cpp
+++ Max_Subarray_Sum/Max_Subarray_Sum/main.cpp
@@ -14,98 +14,40 @@ void max_sub(int[], int n);
 
 void max_sub(int array[], int n)
 {
-	int index1=0;
-	int index2=0;
 	cout<<"\n";
 	int newsum=array[0];
-    int subsum1=array[0];
-	int subsum2=array[0];
     int maxVal=array[0];
-    for(int i=1;i<SIZE;i++)
+
+	int begin=0;
+	int end=0;
+	for(int i=1;i<SIZE;i++)
 	{
-		//newsum starts of as the value in index 0
-		//we add the value in index 1 to newsum
+		//newsum starts of as the value in index 0, we add the value in index 1 to newsum
 		//we compare their sum with the sum of the subarray: index1+index2
 		//the highest value gets placed in newsum
 		newsum=max(newsum+array[i],array[i]+array[i++]);
-		if(newsum+array[i] > array[i]+array[i++])
+		
+		//newsum=max(array[i],newsum+array[i]);
+		//maxVal = max(newsum+array[i++],array[i]+array[i++]);
+		
+		//now we compare maxval with newsum and place the highest value into maxVal and repeat
+		maxVal= max(newsum,maxVal);
+		
+		//track the beginning and ending of the max subarray sum
+		//NOT WORKING
+		if(newsum>0 && newsum>maxVal)
 		{
-			//index0 + index1 - MAX
-			newsum=(newsum+array[i]);
-			//index1+index2 - not max
-			subsum2=(array[i]+array[i++]);
+			end=i;
 		}
 		else
 		{
-			//index1 + index2 - MAX
-			newsum=(array[i]+array[i++]);
-			//index0 + index1 - not max
-			subsum2=(newsum+array[i]);
+			begin=0;
+			end=i;
 		}
-
-		//newsum is the current max of the sum of the subarrays
-		//add the value of the next index into newsum
-		//compare it with the other subarray plus the next index since maybe it will become the largest sum subarray after that
-		maxVal = max(newsum+array[i++],subsum2+array[i++]);
-		//now we compare maxval with newsum and place the max of the 2 into maxVal and repeat
-		maxVal= max(maxVal, newsum);
     }
-	 cout<<"\n";
-     cout<< maxVal;
-	 cout<<"\n";
-
-
-	//for(int i=1;i<SIZE;i++)
-	//{
-	//	newsum starts of as the value in index 0
-	//	we add the value in index 1 to newsum
-	//	we compare their sum with the sum of the subarray: index1+index2
-	//	the highest value gets placed in newsum
-	//	newsum=max(newsum+array[i],array[i]+array[i++]);
-
-	//	newsum is 
-	//	maxVal = max(newsum+array[i++],array[i]+array[i++]);
-	//	now we compare maxval with newsum and place the highest value into maxVal and repeat
-	//	maxVal= max(maxVal, newsum);
-
- //   }
-
-	//for(int i=0; i<SIZE;i++)
-	//{
-	//	index1 = array[i];
-	//	cout<<" "<<index1;
-	//}
-	//cout<<"\n";
-	//for(int j=1; j<SIZE;j++)
-	//{
-	//	index2 = array[j];
-	//	cout<<" " <<index2;
-	//}
-	//int sum=0;
-	//int sum2=0;
-	//int k=0;
-	//while(k<=SIZE)
-	//{
-	//	for(int i=0; i<2;i++)
-	//	{
-	//		sum += array[i];
-	//		cout<<" "<<sum;
-	//	}
-	//	for(int i=1; i<3;i++)
-	//	{
-	//		sum2 += array[i];
-	//		cout<<" "<<sum2;
-	//	}
-	//	if(sum>sum2)
-	//	{
-
-	//	}
-	//	//sum2>sum
-	//	else
-	//	{
-	//		//sum2
-	//	}
-	//}
+	cout<<maxVal<<"\n";
+	cout<<"Starting index is: "<<begin<<"\n";
+	cout<<"Ending index is: " <<end<<"\n";
 }
 
 
